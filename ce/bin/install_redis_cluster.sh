93aef660888a98dfc27bd6f2bcfad8d1f45725d2
synonez <scz.nor@gmail.com>
2023-07-07 17:24:49 +0800

    add:redis_cluster 部署逻辑

--- ce/bin/install_redis_cluster.sh
+++ ce/bin/install_redis_cluster.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+# 用途：用于安装单节点 Redis Cluster
 
 # 安全模式
 set -euo pipefail
@@ -11,7 +12,10 @@ EXITCODE=0
 # 全局默认变量
 REDIS_VERSION="5.0.9"
 BIND_ADDR="127.0.0.1"
-PORT=6379
+
+# redis cluster 端口起始
+PORTS=("6380" "6381" "6382")
+
 DATA_DIR="/var/lib/redis"
 LOG_DIR="/var/log/redis"
 PASSWORD=
@@ -22,7 +26,7 @@ usage () {
 用法:
     $PROGRAM [ -h --help -?  查看帮助 ]
             [ -n, --name        [必填] "部署的 redis_cluster 实例名称" ]
-            [ -p, --port        [必填] "部署的 redis_cluster 端口号" ]
+            [ -p, --port        [必填] "部署的 redis_cluster 端口号列表，多个使用英文逗号分隔" ]
             [ -a, --password    [必填] "部署的 redis_cluster 密码" ]
             [ -b, --bind        [可选] "监听的网卡地址,默认为 127.0.0.1" ]
 
@@ -67,7 +71,7 @@ while (( $# > 0 )); do
             ;;
         -p | --port )
             shift
-            PORT=$1
+            PORTS=( $1 )
             ;;
         -a | --password )
             shift
@@ -97,13 +101,17 @@ fi
 if [[ "$PASSWORD" =~ , ]]; then
     warning "redis_cluster 的密码字符串包含非法字符(逗号)"
 fi
-if ! [[ "$PORT" =~ [0-9]+ ]]; then # 其实要判断是否在1<port<65545之间
-    warning "redis_cluster 的端口不是字符串"
+
+if [[ -z ${PORTS[@]} ]];then
+    error "redis cluster ports列表不能为空"
 else
-    if [[ $(ss -tnl4 | awk -v addr="^${BIND_ADDR}:${PORT}$" '$4 ~ addr' | wc -l) -ge 1 ]]; then
-        warning "${BIND_ADDR}:${PORT} 已经监听，请确认"
-    fi
+    for port in ${PORTS[@]}; do
+        if [[ $(ss -tnl4 | awk -v addr="^${BIND_ADDR}:${port}$" '$4 ~ addr' | wc -l) -ge 1 ]]; then
+            warning "${BIND_ADDR}:${port} 已经监听，请确认"
+        fi
+    done
 fi
+
 if ! [[ "$NAME" =~ ^[a-z] ]]; then
     warning "redis_cluster 实例的名称请用小写字符开头"
 fi
@@ -120,52 +128,48 @@ fi
 install -dv /etc/redis/ ${LOG_DIR} ${DATA_DIR}
 
 # 生成 redis_cluster 配置文件
-CONF_NAME=${NAME}.conf
-log "生成 /etc/redis/$CONF_NAME 配置文件"
-cat > /etc/redis/"${CONF_NAME}" <<EOF
+for port in ${PORTS[@]}; do
+    name="$NAME-$port"
+    CONF_NAME=redis_cluster.conf
+
+    # 创建配置文件目录
+    install -dv /etc/redis/"$port"
+    install -m 755 -o redis -g redis -d ${DATA_DIR}/"${name}"
+
+    cat > /etc/redis/"$port"/"${CONF_NAME}" <<EOF
 daemonize no
-pidfile /var/run/redis-cluster/${NAME}.pid
-port ${PORT}
-cluster-announce-port ${PORT}
-cluster-announce-bus-port 1${PORT}
+pidfile /var/run/redis-cluster/${name}.pid
+port ${port}
+cluster-announce-port ${port}
+cluster-announce-bus-port 1${port}
 bind ${BIND_ADDR}
 cluster-announce-ip ${BIND_ADDR}
 timeout 360
 loglevel notice
-logfile ${LOG_DIR}/${NAME}.log
+logfile ${LOG_DIR}/${name}.log
 tcp-backlog 511
 databases 16
-dbfilename ${NAME}.dump.rdb
-dir ${DATA_DIR}/${NAME}
+dbfilename ${name}.dump.rdb
+dir ${DATA_DIR}/${name}
 requirepass ${PASSWORD}
 masterauth ${PASSWORD}
 cluster-enabled yes
 cluster-node-timeout 15000
-cluster-config-file nodes-${PORT}.conf
+cluster-config-file nodes-$port.conf
 appendonly yes
 EOF
 
-chown redis.redis /etc/redis/"${CONF_NAME}"
 
-# 创建dir目录
-install -m 755 -o redis -g redis -d ${DATA_DIR}/"${NAME}"
-
-cat <<EOF > /usr/lib/systemd/system/redis-cluster.target
-Description=Redis cluster to allow start/stop all redis-cluster.service at once
-
-[Install]
-WantedBy=multi-user.target redis-cluster.target
-EOF
-
-cat <<EOF > /usr/lib/systemd/system/redis-cluster.service
+    # 生成 redis cluster systemd 托管文件
+    cat <<EOF > /usr/lib/systemd/system/redis-cluster-"$port".service
 [Unit]
 Description=Redis Cluster
 After=network-online.target
 PartOf=blueking.target redis-cluster.target
 
 [Service]
-ExecStart=/usr/bin/redis-server /etc/redis/$CONF_NAME
-WorkingDirectory=${DATA_DIR}/${NAME}
+ExecStart=/usr/bin/redis-server /etc/redis/$port/$CONF_NAME
+WorkingDirectory=${DATA_DIR}/${name}
 KillMode=process
 Restart=always
 RestartSec=3s
@@ -175,20 +179,30 @@ LimitNOFILE=204800
 WantedBy=blueking.target redis-cluster.target
 EOF
 
-log "启动 redis-cluster.service 实例"
-systemctl daemon-reload
-systemctl start redis-cluster.service
+    chown redis.redis /etc/redis/"$port"/$CONF_NAME
 
-log "检查 redis-cluster.service 状态"
-if ! systemctl status redis-cluster.service; then
-    log "请检查启动日志，使用命令：journalctl -u redis-cluster.service 查看失败原因"
-    log "手动修复后，使用命令：systemctl start redis-cluster.service 启动并确认是否启动成功"
-    log "启动成功后，使用命令：systemctl enable redis-cluster.service 设置开机启动"
-    exit 100
-else
-    log "设置Redis实例 redis-cluster.service 开机启动"
-    systemctl enable redis-cluster.service
-fi
+    log "启动 redis-cluster-""$port"".service 实例"
+    systemctl daemon-reload
+    systemctl start redis-cluster-"$port".service
+
+    log "检查 redis-cluster-$port.service 状态"
+    if ! systemctl status redis-cluster-"$port".service; then
+        log "请检查启动日志，使用命令：journalctl -u redis-cluster-$port.service 查看失败原因"
+        log "手动修复后，使用命令：systemctl start redis-cluster-$port.service 启动并确认是否启动成功"
+        log "启动成功后，使用命令：systemctl enable redis-cluster-$port.service 设置开机启动"
+        exit 100
+    else
+        log "设置Redis实例 redis-cluster-$port.service 开机启动"
+        systemctl enable redis-cluster-$port.service
+    fi
+done
+
+cat <<EOF > /usr/lib/systemd/system/redis-cluster.target
+Description=Redis cluster to allow start/stop all redis-cluster-*.service at once
+
+[Install]
+WantedBy=multi-user.target redis-cluster.target
+EOF
 
 log "启动 redis-cluster.target 实例"
 systemctl daemon-reload
